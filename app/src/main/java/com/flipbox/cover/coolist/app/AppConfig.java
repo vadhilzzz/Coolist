0afae8946e75210e064d2b49dcab8f17141ca65b
fadhil <fadhilmeist@gmail.com>
2016-03-24 17:18:14 +0700

    URl API bangkit

--- app/src/main/java/com/flipbox/cover/coolist/app/AppConfig.java
+++ app/src/main/java/com/flipbox/cover/coolist/app/AppConfig.java
@@ -6,19 +6,21 @@ package com.flipbox.cover.coolist.app;
  * twitter @mistiawanagus
  */
 public class AppConfig {
-    public static String URL_LOGIN = "http://contact.sakadigital.id/api/users/login";
+    public static String URL_LOGIN = "http://192.168.1.11/api/users/login";
 
-    public static String URL_REGISTER = "http://contact.sakadigital.id/api/users/";
+    public static String URL_REGISTER = "http://192.168.1.11/api/users/list";
 
-    public static String URL_COMPANY = "http://contact.sakadigital.id/api/companies";
+    public static String URL_COMPANY = "http://192.168.1.1/api/companies/list";
 
-    public static String URL_ROLE = "http://contact.sakadigital.id/api/roles";
+    public static String URL_ROLE = "http://192.168.1.1/api/roles/list";
 
-    public static String URL_STATUS = "http://contact.sakadigital.id/api/status_types";
+    public static String URL_STATUS = "http://192.168.1.1/api/status_types";
 
-    public final static String URL_CONTACT = "http://contact.sakadigital.id/api/users?company_id=";
+    public final static String URL_CONTACT = "http://192.168.1.1/api/users/list?companies_id=";
 
-    public static String URL_PROFILE = "http://contact.sakadigital.id/api/users?id=";
+    public static String URL_PROFILE = "http://192.168.1.11/api/users/find?id=";
 
-    public static String URP_REG_FIRST = "http://contact.sakadigital.id/api/users";
+    public static String URP_REG_FIRST = "http://192.168.1.11/api/users/list";
+
+
 }
