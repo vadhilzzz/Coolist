0b16857569a17b6e36c7d1724c35df478ca7078f
fadhil <fadhilmeist@gmail.com>
2016-04-11 17:32:30 +0700

    APK

--- app/src/main/java/com/flipbox/cover/coolist/app/AppConfig.java
+++ app/src/main/java/com/flipbox/cover/coolist/app/AppConfig.java
@@ -6,21 +6,21 @@ package com.flipbox.cover.coolist.app;
  * twitter @mistiawanagus
  */
 public class AppConfig {
-    public static String URL_LOGIN = "http://192.168.1.11/api/users/login";
+    public static String URL_LOGIN = "http://contact.flipbox.co.id/api/users/login";
 
-    public static String URL_REGISTER = "http://192.168.1.11/api/users/list";
+    public static String URL_REGISTER = "http://contact.flipbox.co.id/api/users/list";
 
-    public static String URL_COMPANY = "http://192.168.1.1/api/companies/list";
+    public static String URL_COMPANY = "http://contact.flipbox.co.id/api/companies/list";
 
-    public static String URL_ROLE = "http://192.168.1.1/api/roles/list";
+    public static String URL_ROLE = "http://contact.flipbox.co.id/api/roles/list";
 
-    public static String URL_STATUS = "http://192.168.1.1/api/status_types";
+    public static String URL_STATUS = "http://contact.flipbox.co.id/api/status_types";
 
-    public final static String URL_CONTACT = "http://192.168.1.1/api/users/list?companies_id=";
+    public final static String URL_CONTACT = "http://contact.flipbox.co.id/api/users/list?companies_id=";
 
-    public static String URL_PROFILE = "http://192.168.1.11/api/users/find?id=";
+    public static String URL_PROFILE = "http://contact.flipbox.co.id/api/users/find?id=";
 
-    public static String URP_REG_FIRST = "http://192.168.1.11/api/users/list";
+    public static String URP_REG_FIRST = "http://contact.flipbox.co.id/api/users/list";
 
 
 }
